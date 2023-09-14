ec2eccfa98a738a00d1e72d1155b6ee1c529171d
Narracode <zeronarratore@gmail.com>
2023-09-13 19:10:36 -0600

    Initial commit

---
+++ src/main/java/com/egg/egg_herencia_formas/Egg_Herencia_Formas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.egg.egg_herencia_formas;
+
+/**
+ *
+ * @author zero
+ */
+public class Egg_Herencia_Formas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
